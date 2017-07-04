97ecd3ea320a68972b4a1754ef4f69f66cdc32c2
Kangbo Lu <redstonegranola@gmail.com>
2017-07-04 23:15:37 +0000

    Remove es5 function declaration

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,14 +2,6 @@
 var React = require("react");
 
 // About component definition
-// var About = React.createClass({
-//     render: function() {
-//         return (
-//             <h3>About Component</h3>
-//         );
-//     }
-// });
-
 var About = (props) => {
     return (
         <h3>About Component</h3>
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,18 +2,6 @@
 var React = require("react");
 
 // WeatherMessage component definition
-// var WeatherMessage = React.createClass({
-//     render: function() {
-//         // using destructoring to get props data
-//         var {temp, location} = this.props;
-        
-//         return (
-//             <h3>It's {temp} in {location}</h3>
-//         );
-//     }
-// });
-
-// using destructoring to get props data
 var WeatherMessage = ({temp, location}) => {
     return (
         <h3>It's {temp} in {location}</h3>
